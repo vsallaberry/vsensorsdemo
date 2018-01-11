bad0073c6fb3c812f44e1077ba51ccd51cd29465
vincent <vsallaberry@gmail.com>
2018-01-11 14:21:38 +0100

    [Makefile] remove build.h (generated), add version.h, update libvsensors

--- build.h
+++
@@ -1,3 +0,0 @@
-#define APP_BUILD_NUMBER 163
-#define APP_VERSION 0.1
-#define INCLUDE_SOURCE
---
+++ version.h
@@ -0,0 +1,7 @@
+#ifndef APP_VERSION_H
+#define APP_VERSION_H
+#define APP_VERSION "0.2"
+#define APP_INCLUDE_SOURCE
+#define APP_BUILD_NUMBER 1
+#include "build.h"
+#endif
