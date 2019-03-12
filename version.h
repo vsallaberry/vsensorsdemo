f59fa04b2c71538089c8344ee9266948b916cb00
vincent <vsallaberry@gmail.com>
2019-03-12 21:26:13 +0100

    fix [Makefile] bsdmake objdir fix when switching from curdir to objdir

--- version.h
+++ version.h
@@ -2,7 +2,7 @@
 # define APP_VERSION_H
 # define APP_COMMENT__ "PUT ONLY LINES STARTING WITH '#' IN THIS FILE."
 # undef  APP_COMMENT__
-# define APP_VERSION "0.6.6"
+# define APP_VERSION "0.6.7"
 # define APP_INCLUDE_SOURCE
 # define APP_BUILD_NUMBER 1
 # define DIST_GITREV "unknown"
